d6c37fd4453aeb2da35aa28d45fca5d53dc88119
Tiddo Langerak <tiddolangerak@gmail.com>
2015-05-17 15:17:47 +0200

    Transformed object test to lambda style

--- test/object.js
+++ test/object.js
@@ -1,27 +1,27 @@
 let { get, set } = require('../object');
 
-describe('get', function() {
-	it('should return a property of an object', function() {
+describe('get', () => {
+	it('should return a property of an object', () => {
 		let target = {};
 		let obj = { foo : target };
 		get('foo', obj).should.equal(target);
 	});
 
-	it('should be curryable', function() {
+	it('should be curryable', () => {
 		let obj = { foo : 3 };
 		get('foo')(obj).should.equal(3);
 	});
 });
 
-describe('set', function() {
-	it('should set a property on an object', function() {
+describe('set', () => {
+	it('should set a property on an object', () => {
 		let obj = {};
 		let target = {};
 		set(target, 'foo', obj);
 		obj.foo.should.equal(target);
 	});
 
-	it('should be curryable', function() {
+	it('should be curryable', () => {
 		let obj = {};
 		set(3, 'foo', obj);
 		obj.foo.should.equal(3);
